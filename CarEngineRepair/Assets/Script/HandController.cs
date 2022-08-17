8a62a3135db73b6d4614eb53e075ec95feca3686
Roberto <88709627+bptXR@users.noreply.github.com>
2022-08-17 20:39:17 +0200

    Physics Layer

--- CarEngineRepair/Assets/Script/HandController.cs
+++ CarEngineRepair/Assets/Script/HandController.cs
@@ -6,35 +6,19 @@ namespace Script
     public class HandController : MonoBehaviour
     {
         [SerializeField] private Animator handAnimator;
-        [SerializeField] private InputActionReference triggerActionReference;
-        [SerializeField] private InputActionReference gripActionReference;
-        private static readonly int Trigger = Animator.StringToHash("Trigger");
-        private static readonly int Grab = Animator.StringToHash("Grab");
+        [SerializeField] private InputActionReference triggerActionRef;
+        [SerializeField] private InputActionReference gripActionRef;
 
-        private void OnEnable()
+        private static int _triggerAnimation = Animator.StringToHash("Trigger");
+        private static int _gripAnimation = Animator.StringToHash("Grip");
+
+        private void Update()
         {
-            triggerActionReference.action.performed += Trigger_Performed;
-            triggerActionReference.action.canceled += Trigger_Canceled;
+            float triggerValue = triggerActionRef.action.ReadValue<float>();
+            handAnimator.SetFloat(_triggerAnimation, triggerValue);
 
-            gripActionReference.action.performed += Grip_Performed;
-            gripActionReference.action.canceled += Grip_Canceled;
+            float gripValue = gripActionRef.action.ReadValue<float>();
+            handAnimator.SetFloat(_gripAnimation, gripValue);
         }
-
-        private void OnDisable()
-        {
-            triggerActionReference.action.performed -= Trigger_Performed;
-            triggerActionReference.action.canceled -= Trigger_Canceled;
-            
-            gripActionReference.action.performed -= Grip_Performed;
-            gripActionReference.action.canceled -= Grip_Canceled;
-        }
-
-        private void Trigger_Performed(InputAction.CallbackContext obj) => handAnimator.SetBool(Trigger, true);
-
-        private void Trigger_Canceled(InputAction.CallbackContext obj) => handAnimator.SetBool(Trigger, false);
-
-        private void Grip_Performed(InputAction.CallbackContext obj) => handAnimator.SetBool(Grab, true);
-
-        private void Grip_Canceled(InputAction.CallbackContext obj) => handAnimator.SetBool(Grab, false);
     }
 }
