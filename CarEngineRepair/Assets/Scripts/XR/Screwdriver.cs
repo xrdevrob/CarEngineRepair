f28fe9db225e6830bc15ca60c2afb72c87ec1fcf
Roberto <88709627+bptXR@users.noreply.github.com>
2022-08-19 13:35:53 +0200

    Screw

---
+++ CarEngineRepair/Assets/Scripts/XR/Screwdriver.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace XR
+{
+    public class Screwdriver : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference gripActionRight;
+        [SerializeField] private InputActionReference gripActionLeft;
+        
+        [SerializeField] private InputActionReference triggerActionRight;
+        [SerializeField] private InputActionReference triggerActionLeft;
+        
+        
+        
+    }
+}
